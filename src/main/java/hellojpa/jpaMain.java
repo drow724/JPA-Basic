7d57ae992ea7efc810b7d18e17abbd764f0816d7
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-06-28 20:58:35 +0900

    JPQL

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -1,11 +1,14 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class jpaMain {
+	
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		
@@ -15,9 +18,12 @@ public class jpaMain {
 		tx.begin();
 		
 		try {
-			Member findMember = em.find(Member.class, 1L);
-			findMember.setName("HelloJPA");
+//			Member findMember = em.find(Member.class, 1L);
+			List<Member> result = em.createQuery("select m from Member as m", Member.class).setFirstResult(1).setMaxResults(10).getResultList();
 			
+			for(Member member : result) {
+				System.out.println("member.name = " + member.getName());
+			}
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
