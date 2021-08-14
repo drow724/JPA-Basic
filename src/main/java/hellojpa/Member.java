bb42d959939706713e1c3d968d69cc039463710a
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-08-14 22:40:09 +0900

    객체와 테이블 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,11 +1,11 @@
 package hellojpa;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Table(name = "MBR")
 public class Member {
 	
 	@Id
