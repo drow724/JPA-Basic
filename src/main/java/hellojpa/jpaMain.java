c94edb11bef2a7d634bc1e29e2fa96072346d210
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-09-11 20:23:11 +0900

    기본 키 매핑

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -17,12 +17,26 @@ public class jpaMain {
 		
 		try {
 			
-			Member member = new Member();
-			member.setId(4L);
-			member.setUsername("A");
-			member.setRoleType(RoleType.GUEST);
+			Member member1 = new Member();
+			member1.setUsername("A");
 			
-			em.persist(member);
+			Member member2 = new Member();
+			member2.setUsername("B");
+			
+			Member member3 = new Member();
+			member3.setUsername("C");
+			
+			System.out.println("----------------");
+			
+			em.persist(member1); // 1, 51
+			em.persist(member2); // MEM
+			em.persist(member3); // MEM
+			
+			System.out.println("member.id = " + member1.getId());
+			System.out.println("member.id = " + member2.getId());
+			System.out.println("member.id = " + member3.getId());
+			
+			System.out.println("----------------");
 			
 			tx.commit();
 			
