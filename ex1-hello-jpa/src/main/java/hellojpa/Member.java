c287bfb691fe4e5dc20fe895205ab24605d9c429
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-09-13 22:23:57 +0900

    다대일 [N:1]

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -18,9 +18,6 @@ public class Member {
 	
 	@Column(name = "USERNAME")
 	private String userName;
-	
-//	@Column(name = "TEAM_ID")
-//	private Long teamId;
 
 	@ManyToOne
 	@JoinColumn(name = "TEAM_ID")
