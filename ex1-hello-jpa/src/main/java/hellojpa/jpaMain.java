82e06f7be1645541f4c505ccc882826897958256
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-09-24 00:18:14 +0900

    영속성 전이

--- ex1-hello-jpa/src/main/java/hellojpa/jpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/jpaMain.java
@@ -1,15 +1,9 @@
 package hellojpa;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnitUtil;
-
-import org.hibernate.Hibernate;
 
 public class jpaMain {
 	
@@ -23,33 +17,23 @@ public class jpaMain {
 		
 		try {
 			
-			Team team = new Team();
-			team.setName("teamA");
-			em.persist(team);
-
-			Team teamB = new Team();
-			team.setName("teamB");
-			em.persist(teamB);
+			Child child1 = new Child();
+			Child child2 = new Child();
 			
-			Member member1 = new Member();
-			member1.setUserName("hello1");
-			member1.setTeam(team);
-			em.persist(member1);
-	
-			Member member2 = new Member();
-			member2.setUserName("hello2");
-			member2.setTeam(teamB);
-			em.persist(member2);
+			Parent parent = new Parent();
+			parent.addChild(child1);
+			parent.addChild(child2);
+ 
+			em.persist(parent);
+			em.persist(child1);
+			em.persist(child2);
 			
 			em.flush();
 			em.clear();
 			
-//			Member m = em.find(Member.class, member1.getId());
+			Parent findParent = em.find(Parent.class, parent.getId());
 			
-			List<Member> members = em.createQuery("select m from Member m join fetch m.team" , Member.class).getResultList();
-			
-			//SQL : select * from Member
-			//SQL : select * from Team where TEAM_ID = xxx
+			em.remove(findParent);
 			
 			tx.commit();
 			
