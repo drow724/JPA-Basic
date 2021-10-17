31439bdbf7d7859c39d114fcbc870ec6d85b9bea
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-10-17 18:37:33 +0900

    조건식(CASE 등등)

--- ex3-jpql/src/main/java/jpql/JpaMain.java
+++ ex3-jpql/src/main/java/jpql/JpaMain.java
@@ -26,7 +26,7 @@ public class JpaMain {
 			em.persist(team);
 			
 			Member member = new Member();
-			member.setUsername("member1");
+			member.setUsername(null);
 			member.setAge(10);
 			member.setType(MemberType.ADMIN);
 			
@@ -35,26 +35,25 @@ public class JpaMain {
 			em.flush();
 			em.clear();
 
-//			String query = "select m.username, 'HELLO', true from Member m where m.type = jpql.MemberType.ADMIN";
-//			String query = "select m.username, 'HELLO', true from Member m where m.type = :userType";
-			
-//			String query = "select m.username, 'HELLO', true from Member m where m.username is not null";
-			String query = "select m.username, 'HELLO', true from Member m where m.age between 0 and 10 ";
-			
-			List<Object[]> result = em.createQuery(query)
-			.setParameter("userType", MemberType.ADMIN)
+			//case 식
+//			String query = 	"select " +
+//							"case when m.age <= 10 then '학생요금' " +
+//							"	  when m.age >= 60 then '경로요금' " +
+//							"	  else '일반요금' " +
+//							"end " +
+//							"from Member m";
+//			em.createQuery(query, String.class)
+//			.getResultList();
+		
+			String query = 	"select coalesce(m.username, '이름 없는 회원') as username from Member m";
+			List<String> result = em.createQuery(query, String.class)
 			.getResultList();
 			
-			for (Object[] object : result) {
-				System.out.println("object = " + object[0] );
-				System.out.println("object = " + object[1] );
-				System.out.println("object = " + object[2] );
+			for(String s : result){
+				System.out.println("s = " + s);
 			}
 			
 			tx.commit();
-			
-			
-			
 		} catch (Exception e) {
 			
 			tx.rollback();
