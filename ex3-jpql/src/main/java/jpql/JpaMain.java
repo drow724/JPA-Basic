fddabfb8499ee698d01f0a78d3e4db8028e0c5c0
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-10-24 12:46:33 +0900

    벌크 연산

--- ex3-jpql/src/main/java/jpql/JpaMain.java
+++ ex3-jpql/src/main/java/jpql/JpaMain.java
@@ -30,29 +30,39 @@ public class JpaMain {
 			
 			Member member1 = new Member();
 			member1.setUsername("관리자");
+			member1.setAge(0);
 			member1.setTeam(teamA);
 			em.persist(member1);
 		
 			Member member2 = new Member();
 			member2.setUsername("관리자");
+			member2.setAge(0);
 			member2.setTeam(teamA);
 			em.persist(member2);
 			
 			Member member3 = new Member();
 			member3.setUsername("관리자");
+			member3.setAge(0);
 			member3.setTeam(teamB);
 			em.persist(member3);
 			
-			em.flush();
+			//flush 자돋 호출
+			int resultCount = em.createQuery("update Member m set m.age = 20").executeUpdate();
+			System.out.println("resultCount = " + resultCount);
+			
+			//영속성 컨텍스트 초기화 전
+//			System.out.println("member1.getAge() = " + member1.getAge());
+//			System.out.println("member2.getAge() = " + member2.getAge());
+//			System.out.println("member3.getAge() = " + member3.getAge());
+			
+//			Member findMember = em.find(Member.class, member1.getId());
+//			System.out.println(findMember);
+			
+			//영속성 컨텍스트 초기화
 			em.clear();
 			
-			List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-				.setParameter("username", "관리자")
-				.getResultList();
-			
-			for(Member member : resultList) {
-				System.out.println("member = " + member);
-			}
+			Member findMember = em.find(Member.class, member1.getId());
+			System.out.println(findMember);
 			
 			tx.commit();
 		} catch (Exception e) {
